b052fa41c60f5568cc6245d4114ef1e7875657db
Matt Moretti <werebus@gmail.com>
2015-05-29 11:18:44 -0400

    Robocop linting

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -3,19 +3,19 @@
 # Spec:: default
 #
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2015 UMass Transit Service
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,13 +24,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
 require 'spec_helper'
 
 describe 'passenger::default' do
-
   context 'When all attributes are default, on an unspecified platform' do
-
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
@@ -39,11 +36,9 @@ describe 'passenger::default' do
     it 'does nothing' do
       expect(chef_run.resource_collection).to be_empty
     end
-
   end
 
   context 'On an Ubuntu system' do
-
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
       runner.converge(described_recipe)
@@ -55,7 +50,6 @@ describe 'passenger::default' do
   end
 
   context 'On a CentOS system' do
-
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.0')
       runner.converge(described_recipe)
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,7 @@
 require 'spec_helper'
 
 describe 'passenger::default' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
   it 'does something' do
     skip 'Replace this with meaningful tests'
   end
-
 end
